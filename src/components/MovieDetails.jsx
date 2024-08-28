2f25a493079641376db6c516704f2112876edc4c
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-08-28 17:16:11 +0200

    Mi sono dimenticato di mettere i commenti

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -7,10 +7,12 @@ export default function MovieDetails() {
   const params = useParams();
   const key = "baf3ccdf";
   const [myFilm, setMyFilm] = useState({});
+  const [comments, setComments] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     getFilm();
-  }, [params.movieId]);
+    getComment()
+  }, []);
 
   const getFilm = async () => {
     try {
@@ -24,6 +26,19 @@ export default function MovieDetails() {
       console.log("Errore");
     }
   };
+
+  const getComment = async () => {
+    try{
+        const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${params.movieId}`, {
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmNjODA2ZWZkZWUzZDAwMTU5YmRmMjYiLCJpYXQiOjE3MjQ2NzgyNTQsImV4cCI6MTcyNTg4Nzg1NH0.P-hqBUJZwCk0XSIfjVIJyVCPFhQ0N5RJevlD1GXuKiY"}
+           })
+        const tempCom = await response.json()
+        setComments(tempCom)
+        
+    }
+    catch{console.log("Errore");}
+  }
 
   if (isLoading){
     return  <Spinner className=" m-2" animation="border" variant="info" />
@@ -39,6 +54,13 @@ export default function MovieDetails() {
             <ListGroup.Item variant="dark">{myFilm.Year}</ListGroup.Item>
             <ListGroup.Item variant="dark">{myFilm.Genre}</ListGroup.Item>
             <ListGroup.Item variant="dark">{myFilm.Director}</ListGroup.Item>
+        </ListGroup>
+        
+        <ListGroup className="ms-5">
+        <h4 className=" text-white">Commenti</h4>
+        {comments.map((comment)=> {
+            return <ListGroup.Item variant="dark">{comment.comment}</ListGroup.Item>
+        })}
         </ListGroup>
         </div>
       </div>
